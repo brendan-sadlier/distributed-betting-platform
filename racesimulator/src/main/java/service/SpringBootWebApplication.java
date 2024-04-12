234a1a5557a868f03cb6c61f23d8daf54a99b742
Liam Smyth <liam.smyth@ucdconnect.ie>
2024-04-12 15:53:42 +0100

    creating springboot application and controllers for Race Simulator service

---
+++ racesimulator/src/main/java/service/SpringBootWebApplication.java
@@ -0,0 +1,55 @@
+package service;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class SpringBootWebApplication extends SpringBootServletInitializer implements CommandLineRunner {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWebApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        while (true){
+            try{
+                String service_host = args[0];
+                String bookie_host = args[1];
+                String bookie_port = args[2];
+                String raceSimulatorServiceUrl = "http://"+getHost(service_host)+"/races";
+                RestTemplate template = new RestTemplate();
+                ResponseEntity<String> response = template.postForEntity("http://"+bookie_host+":"+bookie_port+"/services", raceSimulatorServiceUrl, String.class);
+                if (response.getStatusCode().equals(HttpStatus.CREATED)) {
+                    System.out.println("Successfully connected to Bookie");
+                    break;
+                }
+            }catch (Exception e){
+                System.out.println("Failed to connect to Bookie. Will try again in 1 second");
+                Thread.sleep(1000);
+            }
+        }
+    }
+
+    @Value("${server.port}")
+    private int port;
+
+    private String getHost(String service_host) {
+        try {
+            return service_host + ":" + port;
+        } catch (Exception e) {
+            return "localhost:" + port;
+        }
+    }
+}
--- racesimulator/src/main/java/service/controllers/RaceSimulatorController.java
+++ racesimulator/src/main/java/service/controllers/RaceSimulatorController.java
@@ -7,33 +7,34 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import service.core.Horse;
 import service.core.Race;
-import service.oddsbuilder.OddsBuilder;
+import service.racesimulator.RaceSimulator;
 
 @RestController
-public class OddsBuilderController {
-    private final Map<String, Race> races = new TreeMap<>();
-    private final OddsBuilder oddsBuilder = new OddsBuilder();
+public class RaceSimulatorController {
+    private final Map<String, Horse[]> races = new TreeMap<>();
+    private final RaceSimulator simulator = new RaceSimulator();
 
-    @PostMapping(value="/odds", consumes="application/json")
-    public ResponseEntity<Race> createOdds(@RequestBody Race race) {
-        Race raceWithOdds = oddsBuilder.generateOdds(race);
-        races.put(race.dateAndTime.toString(), race);
-        String url = "http://" + getHost() + "/odds/" + raceWithOdds.dateAndTime.toString();
+    @PostMapping(value="/races", consumes="application/json")
+    public ResponseEntity<Horse[]> simulateRace(@RequestBody Race race) {
+        Horse[] winners = simulator.simulateRace(race);
+        races.put(race.dateAndTime.toString(), winners);
+        String url = "http://" + getHost() + "/races/" + race.dateAndTime.toString();
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .header("Location", url)
                 .header("Content-Location", url)
-                .body(raceWithOdds);
+                .body(winners);
     }
 
-    @GetMapping(value="/odds/{raceTime}", produces={"application/json"})
-    public ResponseEntity<Race> getOdds(@PathVariable String raceTime) {
-        Race race = races.get(raceTime);
-        if (race == null) {
+    @GetMapping(value="/races/{raceTime}", produces={"application/json"})
+    public ResponseEntity<Horse[]> getRace(@PathVariable String raceTime) {
+        Horse[] winners = races.get(raceTime);
+        if (winners == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-        return ResponseEntity.status(HttpStatus.OK).body(race);
+        return ResponseEntity.status(HttpStatus.OK).body(winners);
     }
 
     @Value("${server.port}")
