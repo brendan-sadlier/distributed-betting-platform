01e8f57727dd0f316dcf478b9fd0549b6d09acbe
Liam Smyth <liam.smyth@ucdconnect.ie>
2024-04-19 15:46:09 +0100

    editing the bookie controller

--- bookie/src/main/java/service/controllers/BookieController.java
+++ bookie/src/main/java/service/controllers/BookieController.java
@@ -1,6 +1,5 @@
 package service.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,15 +18,10 @@ import java.util.*;
 public class BookieController {
     @Value("${server.port}")
     private int port;
-    private List<Race> races = new ArrayList<>();
-    private Map<Integer, Bet> currentRaceBets = new HashMap<>();
+    private List<Race> races = new LinkedList<>();
     RestTemplate template = new RestTemplate();
 
-    @Autowired
-    private SimpMessagingTemplate messagingTemplate;
-
-    @Autowired
-    private WebSocketController webSocketController;
+    Map<Integer, Bet> currentRaceBets = new HashMap<>();
 
     @PostMapping(value = "races", consumes = "application/json")
     public ResponseEntity<Horse> getWinner(@RequestBody Race race) {
@@ -40,20 +34,46 @@ public class BookieController {
         return ResponseEntity.status(HttpStatus.OK).body(winners[0]);
     }
 
+    public void getWinner() {
+        Race race = races.get(races.size()-1);
+        String url = "http://" + getHost() + "8081/races/" + race.dateAndTime;
+
+        ResponseEntity<Horse[]> response = template.getForEntity(url, Horse[].class);
+        Horse[] winners = response.getBody();
+        assert winners != null;
+
+        Horse winner = winners[0];
+        int horsePosition = race.horses.indexOf(winner);
+        Double odds = race.horseOdds.get(horsePosition);
+
+        // settle all the bets for this race using the horse winners
+
+        for (Map.Entry<Integer, Bet> clientBets : currentRaceBets.entrySet()){
+            // notify each client that has placed a bet on what amount they won, or if their horse lost
+        }
+
+    }
+
     @GetMapping(value = "races", produces = "application/json")
     public ResponseEntity<Race> getRace() {
         String urlRace = "http:// " + getHost() + "8083/generate-races";
         ResponseEntity<Race> response = template.getForEntity(urlRace, Race.class);
+
         Race race = response.getBody();
+
         assert race != null;
         races.add(race);
-        messagingTemplate.convertAndSend("/topic/raceUpdate", race); // Notify all subscribed clients
+
         return ResponseEntity.ok(race);
     }
 
     @GetMapping(value = "all-races", produces = "application/json")
     public ResponseEntity<List<Race>> getAllRaces() {
         return ResponseEntity.status(HttpStatus.OK).body(races);
+    }
+
+    private Double getReward(Integer betSize, Double odds) {
+        return betSize.doubleValue() * odds;
     }
 
     private String getHost() {
