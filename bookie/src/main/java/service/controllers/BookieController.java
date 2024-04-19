82a2e84713910db77ea2985a6be3edc3f92ca1c6
tommycoogan123 <thomas.coogan@ucdconnect.ie>
2024-04-19 15:03:15 +0100

    Bookie can check clients bet for win

--- bookie/src/main/java/service/controllers/BookieController.java
+++ bookie/src/main/java/service/controllers/BookieController.java
@@ -5,21 +5,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import service.core.Bet;
 import service.core.Horse;
 import service.core.Race;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 @RestController
 public class BookieController {
     @Value("${server.port}")
     private int port;
-    private List<Race> races = new ArrayList<>();
+    private List<Race> races = new LinkedList<>();
     RestTemplate template = new RestTemplate();
 
     @PostMapping(value = "races", consumes = "application/json")
@@ -31,6 +29,30 @@ public class BookieController {
 
         assert winners != null;
         return ResponseEntity.status(HttpStatus.OK).body(winners[0]);
+    }
+
+    @PostMapping(value = "races", consumes = "application/json")
+    public ResponseEntity<String> getWinner(@RequestBody Bet bet) {
+        Race race = races.get(races.size()-1);
+        String url = "http://" + getHost() + "8081/races/" + race.dateAndTime;
+
+        ResponseEntity<Horse[]> response = template.getForEntity(url, Horse[].class);
+        Horse[] winners = response.getBody();
+        assert winners != null;
+
+        Horse winner = winners[0];
+        int horsePosition = race.horses.indexOf(winner);
+        Double odds = race.horseOdds.get(horsePosition);
+
+        String result;
+        if (bet.horse == winner) {
+            result = "Congratulations, your horse was the winner, you won " +
+                     getReward(bet.amount, odds);
+        }
+        else {
+            result = "You lost!";
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     @GetMapping(value = "races", produces = "application/json")
@@ -49,6 +71,10 @@ public class BookieController {
     @GetMapping(value = "all-races", produces = "application/json")
     public ResponseEntity<List<Race>> getAllRaces() {
         return ResponseEntity.status(HttpStatus.OK).body(races);
+    }
+
+    private Double getReward(Integer betSize, Double odds) {
+        return betSize.doubleValue() * odds;
     }
 
     private String getHost() {
