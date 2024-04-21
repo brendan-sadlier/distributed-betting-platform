9d494b0bf62e55932383ff26afc94e8f579c1b12
brendan-sadlier <brendan.sadlier@ucdconnect.ie>
2024-04-21 18:39:47 +0100

    Bug Fixes

--- racegenerator/src/main/java/service/controller/RaceGeneratorController.java
+++ racegenerator/src/main/java/service/controller/RaceGeneratorController.java
@@ -16,6 +16,8 @@ import service.RaceGeneratorService;
 import service.core.Race;
 
 import java.io.DataInput;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -37,12 +39,12 @@ public class RaceGeneratorController {
     @PostMapping(value = "/generate-races", produces = "application/json")
     public ResponseEntity<Race> generateRace() {
 
-        String url = "http://localhost:8082/odds";
+        String urlOdds = "http:// " + getHost() + "8082/generate-races";
         Race race = RGService.generateRace();
         RestTemplate template = new RestTemplate();
         HttpEntity<Race> request = new HttpEntity<>(race);
 
-        ResponseEntity<Race> response = template.postForEntity(url, request, Race.class);
+        ResponseEntity<Race> response = template.postForEntity(urlOdds, request, Race.class);
         Race raceWithOdds = response.getBody();
 
         assert raceWithOdds != null;
@@ -54,6 +56,14 @@ public class RaceGeneratorController {
         List<Horse> horses = RGService.getHorses();
 
         return ResponseEntity.ok(horses);
+    }
+
+    private String getHost() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress() + ":";
+        } catch (UnknownHostException e) {
+            return "localhost";
+        }
     }
 
 }
